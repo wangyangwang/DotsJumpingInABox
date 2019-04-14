02640747fbccc92dfada5f005ba8100386d850fc
Yang Wang <yang.wang.dec@gmail.com>
2019-04-14 15:05:47 -0400

    v1.1

--- Assets/RotateMe.cs
+++ Assets/RotateMe.cs
@@ -4,21 +4,46 @@ using UnityEngine;
 
 public class RotateMe : MonoBehaviour {
 
-    [SerializeField] public Transform aroundWho;
-    [SerializeField] public float angleSpeed;
+    public Transform target;
+    public float speed = 1f;
+    public float area;
 
-    Vector3 offset = new Vector3 ();
-    Vector3 initialPosition;
+    float x1, x2;
+    float y1, y2;
+    float z1, z2;
+    Quaternion originalRot;
 
+    // Use this for initialization
     void Start () {
-        initialPosition = transform.position;
+        x1 = Random.Range (0f, 100f);
+        x2 = Random.Range (0f, 100f);
+        y1 = Random.Range (0f, 100f);
+        y2 = Random.Range (0f, 100f);
+        z1 = Random.Range (0f, 100f);
+        z2 = Random.Range (0f, 100f);
+
+        originalRot = transform.rotation;
     }
 
+    // Update is called once per frame
     void Update () {
-        if (aroundWho == null) return;
-        transform.RotateAround (aroundWho.position, Vector3.up, angleSpeed * Time.deltaTime);
-        transform.LookAt (aroundWho);
+        if (target) {
+            transform.LookAt (target);
+        }
+        
+        float x = Mathf.PerlinNoise (x1, x2) * area - area / 3;
+        float y = Mathf.PerlinNoise (y1, y2) * area - area / 3;
+        float z = Mathf.PerlinNoise (z1, z2) * area - area / 3;
 
-        //   angle += 1f * Time.deltaTime;
+        x1 += speed * Time.deltaTime;
+        x2 += speed * Time.deltaTime;
+        y1 += speed * Time.deltaTime;
+        y2 += speed * Time.deltaTime;
+        z1 += speed * Time.deltaTime;
+        z2 += speed * Time.deltaTime;
+
+        Vector3 nextPos = new Vector3 (x, y, z);
+
+        transform.position = nextPos;
     }
 }
